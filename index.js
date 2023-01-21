e805e9c7115ec3e420e3a028ac47365b21dfcf5e
arun mudiraj <arunmudiraj18@gmail.com>
2023-01-21 15:38:35 +0530

    replacing local storage with crud crud as backend

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var addBtn = document.querySelector("#add")
 var inputs = document.querySelectorAll('input')
 var listExpenses = document.querySelector('#allList')
@@ -11,30 +13,49 @@ function addTask(){
         alert("Enter valid amount and description details in respective fields")
         return
     } 
-    if (localStorage.getItem('expenses')===null){
-        var jsonArray = []
-        jsonArray.push([amount.value,description.value,category.value])
-        localStorage.setItem('expenses',JSON.stringify(jsonArray))
+    // if (localStorage.getItem('expenses')===null){
+    //     var jsonArray = []
+    //     jsonArray.push([amount.value,description.value,category.value])
+    //     localStorage.setItem('expenses',JSON.stringify(jsonArray))
+    // }
+    // else{
+    //     var strArray = localStorage.getItem('expenses')
+    //     jsonArray = JSON.parse(strArray)
+    //     jsonArray.push([amount.value,description.value,category.value])
+    //     localStorage.setItem('expenses',JSON.stringify(jsonArray))
+    // }
+    // update()
+    const expense = {
+        "amount" : amount.value,
+        "description" : description.value,
+        "category" : category.value
     }
-    else{
-        var strArray = localStorage.getItem('expenses')
-        jsonArray = JSON.parse(strArray)
-        jsonArray.push([amount.value,description.value,category.value])
-        localStorage.setItem('expenses',JSON.stringify(jsonArray))
-    }
-    update()
+    axios.post('https://crudcrud.com/api/e0a98c38c01f4df9b00fcb87e344545b/app',{data : expense})
+    .then(res=>{
+        console.log(res)
+        update()
+    })
+    .catch(err=>{confirm.log(err)})
+    
 }
 function update(){
-    if (localStorage.getItem('expenses')===null){
-        return
-    }
+    // if (localStorage.getItem('expenses')===null){
+    //     return
+    // }
     strg = ""
-    strArray = localStorage.getItem('expenses')
-    jsonArray = JSON.parse(strArray)
-    jsonArray.forEach((element,index) => {
-       strg+= `<li> $${element[0]} - ${element[1]} - ${element[2]} - <button onclick="edit(${index})" class="bg-yellow-300 active:opacity-75 border-black border rounded-md px-2">Edit</button> <button onclick="deleted(${index})" class="bg-red-400 active:opacity-75 border-black border rounded-md px-2">Delete</button> </li>`
-    });
-    listExpenses.innerHTML = strg
+    axios.get("https://crudcrud.com/api/e0a98c38c01f4df9b00fcb87e344545b/app")
+    .then(res=>{
+        arr = res.data
+        arr.forEach((element,index) => {
+            strg+= `<li> $${element.data.amount} - ${element.data.description} - ${element.data.category} - <button onclick="edit(${index})" class="bg-yellow-300 active:opacity-75 border-black border rounded-md px-2">Edit</button> <button onclick="deleted(${index})" class="bg-red-400 active:opacity-75 border-black border rounded-md px-2">Delete</button> </li>`
+        });
+        listExpenses.innerHTML = strg
+    })
+    // jsonArray = JSON.parse(strArray)
+    // jsonArray.forEach((element,index) => {
+    //    strg+= `<li> $${element[0]} - ${element[1]} - ${element[2]} - <button onclick="edit(${index})" class="bg-yellow-300 active:opacity-75 border-black border rounded-md px-2">Edit</button> <button onclick="deleted(${index})" class="bg-red-400 active:opacity-75 border-black border rounded-md px-2">Delete</button> </li>`
+    // });
+    // listExpenses.innerHTML = strg
 
 
 }
