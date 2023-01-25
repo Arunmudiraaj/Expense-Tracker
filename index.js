675d7557dc6b39136008157ef7cdf47465a5ef84
arun mudiraj <arunmudiraj18@gmail.com>
2023-01-25 11:49:34 +0530

    final

--- index.js
+++ index.js
@@ -1,133 +1,75 @@
 
-
-
-var addBtn = document.querySelector("#add")
+let url = "https://crudcrud.com/api/8bfa18be1a4c4c1daad27626e09ff537"
+var form = document.querySelector("#form")
 var inputs = document.querySelectorAll('input')
 var listExpenses = document.querySelector('#allList')
 var amount = inputs[0]
 var description = inputs[1]
 var category = document.querySelector('select')
 let userEditId = null
-addBtn.addEventListener('click',addTask)
-update()
-async function addTask(){
-    if(amount.value===""||description.value===""){
-        alert("Enter valid amount and description details in respective fields")
-        return
-    } 
-    const expense = {
-        "amount" : amount.value,
-        "description" : description.value,
-        "category" : category.value
+
+window.addEventListener("DOMContentLoaded",load)
+async function load(){
+  response = await axios.get(url+"/expenses")
+    for(let item=0;item<response.data.length;item++){
+      addtoList(response.data[item])
     }
-    if (userEditId!=null){
-        res = await axios.put('https://crudcrud.com/api/f85ea4b0724a43c0afea8b7f323fd93f/expenses/'+userEditId,{data : expense})
+  
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const obj = {
+        amount : amount.value,
+        description : description.value,
+        category : category.value,
+      }
+      if(userEditId === null){
+        axios
+        .post(url+"/expenses",obj)
+        .then((response) => {addtoList(response.data)
+          console.log(response.data._id)})
+      }
+      else{
+        axios
+        .put(`${url}/expenses/${userEditId}`,obj)
+        // addtoList(obj)
+        .then(addtoList({...obj,_id:userEditId}))
         userEditId = null
-            console.log(res)
-            update()
+      }
+
+})
+
+function addtoList(obj){
+    amount.value = ""
+    description.value = ""
+    category.value = ""
+    let listId =obj.amount+obj.category
+    const list = `<li id = ${listId}> $${obj.amount} - ${obj.description} - ${obj.category} - 
+    <button onclick="edit('${obj.amount}','${obj.description}','${obj.category}','${obj._id}')" class="bg-yellow-300 active:opacity-75 border-black border rounded-md px-2">Edit</button> 
+    <button onclick="deleted('${obj.amount}','${obj._id}','${obj.category}')" class="bg-red-400 active:opacity-75 border-black border rounded-md px-2">Delete</button> </li>`
+    listExpenses.innerHTML =listExpenses.innerHTML + list
+  }
+
+  function deleted(amt,id,category){
+    axios.delete(`${url}/expenses/${id}`)
+    removeItemFromList(amt,category)
+  }
+
+  function removeItemFromList(amount,category){
+    let search = amount+category 
+    let childNodetobedeleted = document.getElementById(search)
+    if (childNodetobedeleted){
+      listExpenses.removeChild(childNodetobedeleted)
     }
-    else{
-        axios.post('https://crudcrud.com/api/f85ea4b0724a43c0afea8b7f323fd93f/expenses',{data : expense})
-    .then(res=>{
-        console.log(res)
-        
-        update()
-    })
-    .catch(err=>{console.log(err)})
-    }
-    
-    
-}
-function update(){
-    // if (localStorage.getItem('expenses')===null){
-    //     return
-    // }
-    
-    axios.get("https://crudcrud.com/api/f85ea4b0724a43c0afea8b7f323fd93f/expenses")
-    .then(res=>{
-        listExpenses.innerHTML = ""
-        arr = res.data
-        arr.forEach((element,index) => {
-            
-            //strg+= `<li> $${element.data.amount} - ${element.data.description} - ${element.data.category} - <button onclick="edit(String(element._id))" class="bg-yellow-300 active:opacity-75 border-black border rounded-md px-2">Edit</button> <button onclick="deleted(${index})" class="bg-red-400 active:opacity-75 border-black border rounded-md px-2">Delete</button> </li>`
-            const child = document.createElement('li')
-            child.textContent = `$${element.data.amount} - ${element.data.description} - ${element.data.category} - `
-            const deletebtn = document.createElement('input')
-            deletebtn.type = 'button'
-            deletebtn.value = 'delete'
-            deletebtn.classList.add('bg-red-400', 'mx-1', 'active:opacity-75', 'border-black', 'border', 'rounded-md', 'px-2', 'hover:cursor-pointer')
-            deletebtn.onclick = ()=>{
-                console.log(element._id)
-                axios.delete("https://crudcrud.com/api/f85ea4b0724a43c0afea8b7f323fd93f/expenses/"+element._id)
-                .then((res)=>{
-                    console.log(res)
-                    update()
-                })
-                .catch((err)=>{
-                    console.log(err)
+  }
 
-                })
-            }
-            
-            
+  function edit(amt,des,category,id){
+    amount.value = amt
+    description.value = des
+    category.value = category
+    removeItemFromList(amt,category)
+    userEditId = id
+    console.log(userEditId)
+  }
 
-            const editbtn = document.createElement('input')
-            editbtn.type = 'button'
-            editbtn.value = 'edit'
-            editbtn.classList.add('bg-yellow-300', 'mx-1', 'active:opacity-75', 'border-black', 'border', 'rounded-md', 'px-2', 'hover:cursor-pointer')
-            editbtn.onclick = ()=>{
-                console.log(element._id)
-                  amount.value = element.data.amount
-                  description.value = element.data.description
-                  category.value = element.data.category
-                  userEditId = element._id
-                  listExpenses.removeChild(child)
-                
-            }
-            child.appendChild(editbtn)
-            child.appendChild(deletebtn)
-            listExpenses.appendChild(child)
-        });
-        
-    })
-    // jsonArray = JSON.parse(strArray)
-    // jsonArray.forEach((element,index) => {
-    //    strg+= `<li> $${element[0]} - ${element[1]} - ${element[2]} - <button onclick="edit(${index})" class="bg-yellow-300 active:opacity-75 border-black border rounded-md px-2">Edit</button> <button onclick="deleted(${index})" class="bg-red-400 active:opacity-75 border-black border rounded-md px-2">Delete</button> </li>`
-    // });
-    // listExpenses.innerHTML = strg
-
-
-}
-// function deleted(id){
-//     console.log("in delete");
-//     // strArray = localStorage.getItem('expenses')
-//     // jsonArray = JSON.parse(strArray)
-//     // jsonArray.splice(index, 1)
-//     // localStorage.setItem('expenses',JSON.stringify(jsonArray))
-//     // update()
-//     axios.delete("https://crudcrud.com/api/fad0845aee04436fa8699151c04c8f8f/expenses/"+id)
-//     .then(res=>{ 
-//         console.log(res)
-//         update()
-//      })
-//     .catch(err=>{console.log(err)})
-// }
-// function edit(id){
-//     console.log("in edit");
-
-//     // strArray = localStorage.getItem('expenses')
-//     // jsonArray = JSON.parse(strArray)
-//     // element = jsonArray[index]
-//     // amount.value = element[0]
-//     // description.value = element[1]
-//     // category.value = element[2]
-//     // deleted(index)
-//     const expense = {
-//         "amount" : amount.value,
-//         "description" : description.value,
-//         "category" : category.value
-//     }
-//     axios.put("https://crudcrud.com/api/fad0845aee04436fa8699151c04c8f8f/expenses/"+id,{data : expense})
-//     .then(res=>{ console.log(res) })
-//     .catch(err=>{console.log(err)})
-// }
